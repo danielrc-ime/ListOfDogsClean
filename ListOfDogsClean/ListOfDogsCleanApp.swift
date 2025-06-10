6a38d138af36adc863086c2a1656a94c1670681c
darc-code <danielrc.ime@gmail.com>
2025-06-10 14:39:23 -0600

    Initial Commit

---
+++ ListOfDogsClean/ListOfDogsCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfDogsCleanApp.swift
+//  ListOfDogsClean
+//
+//  Created by Daniel Alberto Rodriguez Cielo on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ListOfDogsCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
